5c898a2508e468c519cb68d2621189bc384ca76d
S <s>
2012-04-26 19:36:22 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -68,7 +68,9 @@ public class Breakout extends GraphicsProgram {
 				double y = i * (BRICK_HEIGHT + BRICK_SEP) + BRICK_Y_OFFSET;
 				GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
 				add(brick);
+				for (int k = 0; k < NBRICK_ROWS; k *= 2)
 				brick.setFilled(true);
+				brick.setFillColor(Color.RED);
 			}
 		}
 	}
